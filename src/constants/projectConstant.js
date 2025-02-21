82ee9252f1bef119e112f43d7afc9afaf204ed95
Komchan <111962421+itskom@users.noreply.github.com>
2025-02-21 12:01:13 +0000

    Added recent projects

--- src/constants/projectConstant.js
+++ src/constants/projectConstant.js
@@ -1,24 +1,28 @@
 import PortfolioImg from "../assets/portfolio-v1.png";
 import ApplianceNowImg from "../assets/appliancenow.png";
+import Spaces_MMUImg from "../assets/spaces-mmu.png";
 
 const projectsData = [
   {
     id: 1,
     name: "ApplianceNow",
     description: "A Full-Stack Home Appliance E-Commerce Site",
-    techStack: [
-      "JavaScript",
-      "Java",
-      "SQLite",
-      "HTML",
-      "CSS",
-    ],
+    techStack: ["JavaScript", "Java", "SQLite", "HTML", "CSS"],
     image: ApplianceNowImg,
     github: "https://github.com/itskom",
     website: "https://github.com/itskom",
   },
   {
     id: 2,
+    name: "SpacesMMU",
+    description: "Interactive study space finder for MMU Students",
+    techStack: ["NextJS", "Mapbox", "Tailwind", "Vercel"],
+    image: Spaces_MMUImg,
+    github: "https://github.com/itskom/SpacesMMU",
+    website: "https://spaces-mmu.vercel.app/",
+  },
+  {
+    id: 3,
     name: "Portfolio V1",
     description: "A personal portfolio to showcase my projects and skills.",
     techStack: ["JavaScript", "React", "CSS", "NextJS", "Vercel"],
